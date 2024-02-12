f8ab842d5a80b5769c4c68411f1dd533d8725249
athefj <150645620+athefj@users.noreply.github.com>
2024-02-12 10:53:35 -0600

    Update lib.js

--- lib.js
+++ lib.js
@@ -348,8 +348,10 @@ function read_in_bed9_defaults() {
         //    `datasets/${REF}/CHM1_raw.bed9`,
         ],
         query: [
-            `datasets/${REF}/${QUERY}_annot.bed`,
-            `datasets/${QUERY}/PNG15_hap1_hg38_annot.bed`,
+            `datasets/test/PNG15_hap1_asm_annot.bed`,
+            `datasets/test/PNG15_hap1_hg38_annot.bed`,
+        //    `datasets/${REF}/${QUERY}_annot.bed`,
+        //    `datasets/${QUERY}/PNG15_hap1_hg38_annot.bed`,
         //    `datasets/${QUERY}/${QUERY}_dupmasker_colors.bed`,
         //    `datasets/${QUERY}/${QUERY}_gaps.bed`,
         //    `datasets/${QUERY}/${QUERY}_CenSat.bed`,
