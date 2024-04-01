11379847cfb0c0971c346c001dcb351f22c2936d
Athef Javidh <javid017@ahl01.agate.msi.umn.edu>
2024-04-01 12:54:41 -0500

    segdup

--- lib.js
+++ lib.js
@@ -340,7 +340,7 @@ function read_in_bed9_defaults() {
     var bed_files = {
         ref: [
          //   `datasets/${REF}/${REF}_CenSat.bed`,
-         // `datasets/${REF}/${REF}_dupmasker_colors.bed`,
+            `datasets/${REF}/${REF}_dupmasker_colors.bed`,
         //    `datasets/${REF}/${REF}_genes_small.bed`,
             `datasets/${REF}/${REF}_genes.bed`,
         //    `datasets/${REF}/${REF}_gaps.bed`
