f4594cdcfbc8dd772c79e990c5f4c78398758f0b
athefj <150645620+athefj@users.noreply.github.com>
2024-11-07 11:29:20 -0800

    Update SafFire.js

--- SafFire.js
+++ SafFire.js
@@ -78,9 +78,12 @@ function miropeats_d3(data) {
 
     // perid scale
     var yscale_c = d3.scaleLinear()
-        .domain([d3.max([89, d3.min(aln_data, function (d) { return d.id })]),
-        d3.max(aln_data, function (d) { return d.id })])
-        .range([height, height - space_for_bed + 10 - margin.bottom + label_margin]);
+        .domain([0, 1])
+        .range([height, height]);
+    // var yscale_c = d3.scaleLinear()
+    //     .domain([d3.max([89, d3.min(aln_data, function (d) { return d.id })]),
+    //     d3.max(aln_data, function (d) { return d.id })])
+    //     .range([height, height - space_for_bed + 10 - margin.bottom + label_margin]);
 
     // opacity scale
     alpha_scale = d3.scaleLinear()
