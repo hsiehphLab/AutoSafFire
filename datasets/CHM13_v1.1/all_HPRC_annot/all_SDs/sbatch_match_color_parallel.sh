95e55c367d36d0594960f0e9731e1cae94503dc4
Athef Javidh <javid017@ahl03.agate.msi.umn.edu>
2024-12-18 17:22:58 -0600

    corrected ASM SD annot colors

--- datasets/CHM13_v1.1/all_HPRC_annot/all_SDs/sbatch_match_color_parallel.sh
+++ datasets/CHM13_v1.1/all_HPRC_annot/all_SDs/sbatch_match_color_parallel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --time=00:15:00
+#SBATCH --time=02:30:00
 #SBATCH --job-name=annot_color
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks=1
@@ -9,9 +9,11 @@
 #SBATCH --partition=sioux,msismall,msilarge
 
 cd /home/hsiehph/shared/javid017-home/jobs/AutoSafFire/datasets/CHM13_v1.1/all_HPRC_annot/all_SDs/
-sed -e '1 ! s/$/\t\t\t\t\t\t162,162,162/' -i /home/hsiehph/shared/javid017-home/jobs/AutoSafFire/datasets/CHM13_v1.1/all_HPRC_annot/all_SDs/*.bed
+sed -e '1 ! s/$/\t\t\t162,162,162/' -i /home/hsiehph/shared/javid017-home/jobs/AutoSafFire/datasets/CHM13_v1.1/all_HPRC_annot/all_SDs/*.bed
 for FILE in /home/hsiehph/shared/javid017-home/jobs/AutoSafFire/datasets/CHM13_v1.1/all_HPRC_annot/all_SDs/*.bed; do
 	sbatch --partition sioux,msismall,msilarge --ntasks=1 --cpus-per-task=1 --mem=5G --time=04:00:00 match_color.sh "$FILE" &
 done
 
 wait
+
+
