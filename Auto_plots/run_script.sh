aa83e9e1aa017437328efa85d280a27012a572cb
Athef Javidh <javid017@ahl03.agate.msi.umn.edu>
2025-05-13 18:42:42 -0500

    auto v2

--- Auto_plots/run_script.sh
+++ Auto_plots/run_script.sh
@@ -1,14 +1,11 @@
 #!/bin/bash -l
 
-#SBATCH --time=24:00:00
-
+#SBATCH --time=01:00:00
 #SBATCH --ntasks=1
 #SBATCH --mem=5g
 #SBATCH --tmp=1g
 
 source /projects/standard/hsiehph/shared/bin/initialize_conda.sh
 conda activate snakemake
-
-
 
 snakemake  -j 1
