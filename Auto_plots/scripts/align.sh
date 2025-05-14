97707a1ef305ec182999df9449c95d6c85faa3e2
Athef Javidh <javid017@ahl04.agate.msi.umn.edu>
2025-05-14 10:18:25 -0500

    align params updt

--- Auto_plots/scripts/align.sh
+++ Auto_plots/scripts/align.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --time=1:00:00
+#SBATCH --time=00:30:00
 #SBATCH --mem=100g
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=15
 #SBATCH --ntasks=1
 #SBATCH --partition=sioux,msismall,msilarge
 
 module load minimap2/2.26
 
-minimap2 -K 48G -t 10 -c -x asm20 --secondary=no --eqx -s 25000 "$1" "$2" > "$3"
+minimap2 -K 32G -t 15 -c -x asm20 --secondary=no --eqx -s 25000 "$1" "$2" > "$3"
 
